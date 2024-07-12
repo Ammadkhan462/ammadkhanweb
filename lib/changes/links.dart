2b613b68181e26adbe226c41105c5cfe6aaafe41
Ammad khan <160843624+kc-ammad-khan@users.noreply.github.com>
2024-07-12 05:35:07 +0500

    Initial commit of the cleaned project

---
+++ lib/changes/links.dart
@@ -0,0 +1,37 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1Op5FxwNQCaZj4d-zwUDOEU12DCbAZSQQ/view?usp=drive_link';
+
+// Github Link
+const String gitHub = 'https://github.com/Ammadkhan462';
+// Github Link
+const String facebook = 'https://www.facebook.com/ammad.khanniazi.9/';
+// Github Link
+const String twitter = '';
+// Github Link
+const String youtube = '';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/ammadkhans/';
+// Github Link
+const String buymeacoffee = '';
+// Github Link
+const String instagram = 'https://www.instagram.com/ammadkhans.niazi/';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+const String fiverr = ' ';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~012c665014defc0c99';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=3087048462';
