c52ceeec88256e1af6f407926d01597da26b2907
Ammad khan <160843624+kc-ammad-khan@users.noreply.github.com>
2024-08-16 10:58:33 +0500

    Add build files

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -46,9 +46,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e65fd16f82d28d77a2af86508ff9a986
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3b2abd701e5a2acb22ceeacb0482ab05",
-"/": "3b2abd701e5a2acb22ceeacb0482ab05",
-"main.dart.js": "86ea2af7eb4655d57a6555946f4bc7b8",
+"index.html": "864f196af1f4a63cc71c852ee5478a4b",
+"/": "864f196af1f4a63cc71c852ee5478a4b",
+"main.dart.js": "666155336be38ea08ae6ecc14e7070cf",
 "manifest.json": "47e086f8841aee14e325f5f82ed0795f",
 "version.json": "f124181e58b12572810925130e6735b8"};
 // The application shell files that are downloaded before a service worker can
